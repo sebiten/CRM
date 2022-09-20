28cbda43af19950fdfecba18c8718478c075e5ee
Sergio Sebastian burgos <sebamayorde18@gmail.com>
2022-09-20 15:08:52 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import Formulario from "./Components/Formulario";
+import ListadoPacientes from "./Components/ListadoPacientes";
+import Header from "./Components/Header";
+
+function App() {
+  const [pacientes, setPacientes] = useState([]);
+
+  return (
+    <div className="container mx-auto mt-20">
+      <Header />
+
+      <div className="mt-12 md:flex">
+        <Formulario 
+          pacientes={pacientes} 
+          setPacientes={setPacientes} />
+        <ListadoPacientes 
+          pacientes={pacientes}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
